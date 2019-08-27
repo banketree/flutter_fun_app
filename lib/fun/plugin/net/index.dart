ae0a76797e5ffe91a6d358c0f503167e35d530e3
banketree <banketree@qq.com>
2019-08-27 22:12:32 +0800

    net action

---
+++ lib/fun/plugin/net/index.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
+
+Future<bool> isConnected() async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  return connectivityResult != ConnectivityResult.none;
+}
+
+void test() async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  if (connectivityResult == ConnectivityResult.mobile) {
+// 网络类型为移动网络
+  } else if (connectivityResult == ConnectivityResult.wifi) {
+// 网络类型为WIFI
+  }
+}
+
+class NetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
